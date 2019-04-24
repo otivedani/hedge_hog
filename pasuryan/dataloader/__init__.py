14cfad05378101aed0a767327400e844803b2539
Dani <otivedani@outlook.com>
2019-04-24 09:30:44 +0700

    fix true div by 0

--- pasuryan/dataloader/__init__.py
+++
@@ -1,3 +0,0 @@
-from .extended_ck import extended_ck_load
-from .yale import yale_image_load
-from .splitter import splitter, splitter_random
---
+++ pasuryan/datasets/__init__.py
@@ -0,0 +1,3 @@
+from .extended_ck import extended_ck_load
+from .yale import yale_image_load
+from .splitter import splitter, splitter_random
