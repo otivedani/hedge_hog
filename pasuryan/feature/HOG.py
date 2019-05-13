01a081dc66b7cb2a8af60bf07507d39a4b14d928
ItoniaXI <itonia.x.i@gmail.com>
2019-05-13 15:02:04 +0700

    fix on odd sized cell

--- pasuryan/feature/HOG.py
+++ pasuryan/feature/HOG.py
@@ -15,7 +15,8 @@ import numpy as np
 from ..improc import filters
 from ..powerup import indextra
 
-def hog(image2d, cell_size=(8,8), block_size=(2,2), block_stride=(1,1), nbins=9, useSigned=False, useInterpolation=False, normalizeType='L2.old', ravel=True):
+def hog(image2d, cell_size=(8,8), block_size=(2,2), block_stride=(1,1), 
+    nbins=9, useSigned=False, useInterpolation=False, normalizeType='L2.old', ravel=True):
     """
     Parameters : 
 	------------
@@ -37,16 +38,16 @@ def hog(image2d, cell_size=(8,8), block_size=(2,2), block_stride=(1,1), nbins=9,
     """
 
     # Section 0. Precompute variables
+    csize_y, csize_x = cell_size
     h, w = image2d.shape
-    h_incell, w_incell = h//cell_size[0], w//cell_size[1]
+    h_incell, w_incell = h//csize_y, w//csize_x
     #lazy cropping
-    h, w = h_incell*cell_size[0], w_incell*cell_size[1]
+    h, w = h_incell*csize_y, w_incell*csize_x
     image2d = image2d[:h,:w]
-
     degreebase = 180 if not useSigned else 360
         
-    _dum = np.arange(h_incell*w_incell).reshape(h_incell,w_incell).repeat(cell_size[0], axis=1).repeat(cell_size[1], axis=0)
-    _place_in_bin = np.pad(_dum, (cell_size[0]//2,cell_size[1]//2), 'reflect')
+    _dum = np.arange(h_incell*w_incell).reshape(h_incell, w_incell).repeat(csize_x, axis=1).repeat(csize_y, axis=0)
+    _place_in_bin = np.pad(_dum, ((csize_y//2, csize_y//2),(csize_x//2, csize_x//2)), 'edge')
     # print(np.all(_place_to_bin[4:-4,4:-4]==_dum) #must be True)
     
     # Section 1. gradient image x and y
@@ -61,18 +62,16 @@ def hog(image2d, cell_size=(8,8), block_size=(2,2), block_stride=(1,1), nbins=9,
         
         magcoef, angcoef = linterp(ang, nbins, useSigned)
         ang2x = ((nbins-1)*angcoef/degreebase).astype('int')
+        
+        _pibx = np.array((
+            _place_in_bin[:h,:w], \
+            _place_in_bin[:h,-w:], \
+            _place_in_bin[-h:,:w], \
+            _place_in_bin[-h:,-w:]))
 
-        dum = np.pad(_dum, (cell_size[0]//2,cell_size[1]//2), 'reflect')
-        _pibx = np.array((_place_in_bin[:-8,:-8], _place_in_bin[:-8,8:], _place_in_bin[8:,:-8], _place_in_bin[8:,8:]))
-        # _pibx = np.array((_place_in_bin[8:,8:],_place_in_bin[8:,:-8], _place_in_bin[:-8,8:], _place_in_bin[:-8,:-8]))
-        # _pibx = np.array([_dum,_dum,_dum,_dum])
-
-        # # !!!important, if you dont want data loss
-        # mag2x = mag2x.astype(long)
+        # # !!!important, if you dont want data loss (Python 2.x) -> mag2x = mag2x.astype(long)
         magx = blinterp(magcoef*mag[None,:,:],cell_size)
         binx = ang2x[:,None,:,:]+(_pibx*nbins)[None,:,:,:]
-
-        # print(mag2x.shape, ang2x.shape, bin2x.shape, newbin2x.shape, (lemper1*_hog_param_visd['orientations']).shape)
 
     else:
         _bin_step = (degreebase/(nbins-1))
@@ -80,8 +79,7 @@ def hog(image2d, cell_size=(8,8), block_size=(2,2), block_stride=(1,1), nbins=9,
         _pibx = _place_in_bin[cell_size[0]//2:-cell_size[0]//2,cell_size[1]//2:-cell_size[1]//2]
         binx = angx[None, :,:]+(_pibx[None, :,:]*nbins)
         magx = mag[None, :, :]
-        
-
+                
     hists = np.bincount(binx.ravel(), magx.ravel(), h_incell*w_incell*nbins)\
                 .reshape((h_incell,w_incell,nbins))
     
@@ -94,11 +92,8 @@ def hog(image2d, cell_size=(8,8), block_size=(2,2), block_stride=(1,1), nbins=9,
 
     blockhists = hists.ravel()[_blockix]
 
-    if normalizeType=='L2.old':
-        divisor = np.sqrt(np.sum(blockhists.copy()**2, axis=2))[:,:,None]
-        blockhists /= np.where(divisor!=0, divisor, 1)
     #L2-norm
-    elif normalizeType=='L2':
+    if normalizeType=='L2':
         divisor = np.sqrt(np.sum(blockhists.copy()**2, axis=2)+(1e-7)**2)[:,:,None]
         blockhists /= np.where(divisor!=0, divisor, 1)
     #L2-hys
@@ -117,6 +112,13 @@ def hog(image2d, cell_size=(8,8), block_size=(2,2), block_stride=(1,1), nbins=9,
         divisor = np.abs(np.sum(blockhists.copy(), axis=2)+(1e-7))[:,:,None]
         blockhists /= np.where(divisor!=0, divisor, 1)
         blockhists = np.sqrt(blockhists)
+    #make this default normalize method
+    elif normalizeType=='L2.old':
+        divisor = np.sqrt(np.sum(blockhists.copy()**2, axis=2))[:,:,None]
+        blockhists /= np.where(divisor!=0, divisor, 1)
+    #not normalized, no good. debug purpose only.
+    else:
+        pass
         
     if ravel:
         return blockhists.ravel()
@@ -139,20 +141,19 @@ def linterp(ang, nbins, signed=False):
         [x_2,                 x_1]
     """
     degreebase = 180 if not signed else 360
-
+    
     #coefficient
     binStep = degreebase/nbins
-    x = np.copy(ang)
-    x_1 = np.floor(ang/binStep)*binStep
+    x = ang    
+    x_1 = (ang//binStep)*binStep
     c = (x-x_1)/binStep
-    x_2 = np.where(x_1 < 180, x_1+binStep, x_1)
-
+    x_2 = np.where(x_1 < degreebase, x_1+binStep, 0)
     # return np.array([[c*mag, x_2],[(1-c)*mag, x_1]])
+    x_1 = np.where(x_1 < degreebase, x_1, 0)
     return np.array([c,(1-c)]), \
             np.array([x_2, x_1])
     
 def blinterp(mag, cellSize):
-    #FLAG
     """
     Parameters : 
 	------------
@@ -171,8 +172,8 @@ def blinterp(mag, cellSize):
     xi = np.tile(np.arange(mag.shape[-1]), (mag.shape[-2],1))
     yj = np.tile(np.arange(mag.shape[-2]), (mag.shape[-1],1)).T
 
-    xi_i = np.floor((xi+cellSize[0]/2)/cellSize[0])*cellSize[0]
-    yj_j = np.floor((yj+cellSize[1]/2)/cellSize[1])*cellSize[1]
+    xi_i = ((xi+cellSize[0]/2)//cellSize[0])*cellSize[0]
+    yj_j = ((yj+cellSize[1]/2)//cellSize[1])*cellSize[1]
     x_coef = ((xi+cellSize[0]/2)-xi_i)/cellSize[0]
     y_coef = ((yj+cellSize[1]/2)-yj_j)/cellSize[1]
 
@@ -181,12 +182,6 @@ def blinterp(mag, cellSize):
         x_coef*(1-y_coef),\
         (1-x_coef)*y_coef,\
         x_coef*y_coef           ])
-    #
-    # mag[0,0,0] = 0.1234
-    # mag[1,0,0] = 5.6789
-    # print(_blin_coefs.shape, mag.shape)
-#     print(np.all(np.sum(_blin_coefs, axis=(0))==1))
-#     print(np.amax(_blin_coefs), np.amin(_blin_coefs), _blin_coefs.dtype)
     
     """
     mag.shape = (..., height, width)
